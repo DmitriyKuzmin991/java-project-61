0dfd68d8d634207770f73ebdff90f28b14f64527
DmitriyKuzmin <dmitrykuzminn@gmail.com>
2023-03-04 23:57:55 +0300

    add third game and Fix minor bugs

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,49 +4,23 @@ package hexlet.code;
 
 public class App {
     public static void main(String[] args) {
-        String userName = "Player";
-        while (true) {
 
             int resultChoiceGame = Cli.choiceGames();
             System.out.println();
             switch (resultChoiceGame) {
                 case (1):
-                    userName = Cli.greetings();
+                    String userName = Cli.greetingsAndGetName();
                     System.out.println();
-                    continue;
+                    return;
                 case(2):
-                    LibraryOfGames.gameTwo(userName);
+                    LibraryOfGames.gameTwoIsNumberEven();
+                    return;
+                case(3):
+                    LibraryOfGames.gameThreeCalc();
                     return;
                 default:
                     return;
             }
-
-
-
-        }
-
-
-
-
-
-        //scan.close();
-//        if (resultChoiceGame == 0) {
-//            return;
-//        } else if (resultChoiceGame == 1) {
-//
-//        } else {
-//            System.out.println("Try agan");
-//        }
-
-//
-//         switch (resultChoiceGame) {
-//             case (1):
-//                 //start game
-//             case ()
-
-
-
-
 
     }
 }

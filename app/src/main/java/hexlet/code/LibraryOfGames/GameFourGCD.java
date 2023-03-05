3568ba23a7445ccdcb1d5395054c0aa26fbe5c95
DmitriyKuzmin <dmitrykuzminn@gmail.com>
2023-03-05 22:13:37 +0300

    New logic get greatest common divisor

--- app/src/main/java/hexlet/code/LibraryOfGames/GameFourGCD.java
+++ app/src/main/java/hexlet/code/LibraryOfGames/GameFourGCD.java
@@ -13,14 +13,14 @@ public class GameFourGCD {
         while (countRightAnswer < 3) {
             int firstValue = (int) (Math.random() * 100);
             int secondValue = (int) (Math.random() * 100);
-            int halfMaxValue = Math.max(firstValue, secondValue) / 2;
-            int gcd = 0;
-            for (int i = halfMaxValue; i > 0; i--) {
-                if (firstValue % i == 0 && secondValue % i == 0) {
-                    gcd = i;
-                    break;
-                }
+            int maxValue = Math.max(firstValue, secondValue);
+            int minValue = Math.min(firstValue, secondValue);
+            while (minValue != 0){
+                int temp = maxValue % minValue;
+                maxValue = minValue;
+                minValue = temp;
             }
+            int gcd = maxValue;
             System.out.printf("Question: %s and %s", firstValue ,secondValue);
             System.out.print("\nYour answer: ");
             int answer = scan.nextInt();

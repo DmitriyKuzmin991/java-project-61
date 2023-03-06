68fa6a3f25d93293ddbe6afeff31f1c6a59c3c24
DmitriyKuzmin <dmitrykuzminn@gmail.com>
2023-03-06 22:56:15 +0300

    enhanced switch in GameThreeCalc

--- app/src/main/java/hexlet/code/LibraryOfGames/GameThreeCalc.java
+++ app/src/main/java/hexlet/code/LibraryOfGames/GameThreeCalc.java
@@ -12,21 +12,22 @@ public class GameThreeCalc {
             int firstValue = (int) (Math.random() * 100 + 3);
             int secondValue = (int) (Math.random() * 20 + 1);
             int mathSignRandom = (int) (Math.random() * 3 + 1);
-            int correctAnswer = 0;
+            int correctAnswer;
             System.out.println("What is the result of the expression?");
             System.out.print("Question: ");
             switch (mathSignRandom) {
-                case(1):
+                case (1) -> {
                     System.out.println(firstValue + " + " + secondValue);
                     correctAnswer = firstValue + secondValue;
-                    break;
-                case(2):
+                }
+                case (2) -> {
                     System.out.println(firstValue + " - " + secondValue);
                     correctAnswer = firstValue - secondValue;
-                    break;
-                default:
+                }
+                default -> {
                     System.out.println(firstValue + " * " + secondValue);
                     correctAnswer = firstValue * secondValue;
+                }
             }
             System.out.print("Your answer: ");
             Scanner scan = new Scanner(System.in);

5b34a403f13b0ad73d9332cec89eb9bcb4329527
DmitriyKuzmin <dmitrykuzminn@gmail.com>
2023-03-13 22:50:08 +0300

    add final minValue in game isPrime

--- app/src/main/java/hexlet/code/LibraryOfGames/GameSixPrime.java
+++ app/src/main/java/hexlet/code/LibraryOfGames/GameSixPrime.java
@@ -5,7 +5,8 @@ import hexlet.code.GameEngine;
 import java.util.Random;
 
 public class GameSixPrime {
-    private static final int RANGE_RANDOM_VALUE = 137;
+    private static final int MAX_RANDOM_VALUE = 137;
+    private static final int MIN_RANDOM_VALUE = 4;
 
     public static void isPrimeGame() {
         int gameRound = 0;
@@ -13,7 +14,7 @@ public class GameSixPrime {
         String[] rightAnswer = new String[GameEngine.COUNT_OF_ROUND];
         String rules = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
         while (gameRound < GameEngine.COUNT_OF_ROUND) {
-            int randomNumber = new Random().nextInt(RANGE_RANDOM_VALUE);
+            int randomNumber = new Random().nextInt(MAX_RANDOM_VALUE + MIN_RANDOM_VALUE);
             question[gameRound] = "Question: " + randomNumber + "\nYour answer: ";
             rightAnswer[gameRound] = isPrime(randomNumber);
             gameRound++;
@@ -21,12 +22,6 @@ public class GameSixPrime {
         GameEngine.engineApp(rules, question, rightAnswer);
     }
     static String isPrime(int checkingNumber) {
-        if (checkingNumber <= 1) {
-            return "no";
-        }
-        if (checkingNumber <= 3) {
-            return "yes";
-        }
         int maxDiv = checkingNumber / 2;
         for (int i = 2; i <= maxDiv; i++) {
             if (checkingNumber % i == 0) {
